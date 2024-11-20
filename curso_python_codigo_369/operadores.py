2fa54e9d8bf23b47f4205d3a3ec95fd6b4c34170
Jhonatan Araujo <jhona01230123@gmail.com>
2024-11-19 22:15:31 -0400

    nuevo curso de python

---
+++ curso_python_codigo_369/operadores.py
@@ -0,0 +1,29 @@
+# OPERADORES RELACIONALES
+'''
+==      igual que 
+!=      distinto que 
+<       menor que 
+<=      menor o igual que
+>       mayor que 
+>=      mayor o igual que
+
+'''
+# OPERADORES LOGICOS
+'''
+and             si cumple a y b             
+or              si cumple a o b
+not             no 
+'''
+# OPERADORES DE ASIGNACION
+'''
++=              
+'''
+c = 10
+c += 5
+print(c)
+# lo que pasa aqui es: si c es 10 entonces: c = 10 + 5 tomando este el nuevo valor
+
+# la salida de datos se establece con (input) como en el siguiente ejemplo
+
+numero = int(input("ingresa un numero: "))
+print(numero)
