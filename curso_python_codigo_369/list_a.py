5df2522bffb4e17e328d27877979b01abafcd405
Jhonatan Araujo <jhona01230123@gmail.com>
2024-11-25 16:29:45 -0400

    ejericicos

---
+++ curso_python_codigo_369/list_a.py
@@ -0,0 +1,20 @@
+# LISTAS Y ARRAYS
+
+lista = [1, 2, 3, "jhonas", 5.3, ["a", "b", "c"], 5]
+# si queremos llamar se cuenta una desde el 0 en adelante 
+# en este caso llamaremos al segundo numero que seria el 2 
+print(lista[1])
+
+# en este caso llamaremos desde un inicio a un final de la lista
+# en pantalla nos muestra desde el 3 hasta jhonas
+print(lista[2:4])
+
+# si colocamos el solo el inicio imprimira desde donde iniciara 
+# hasta el final 
+print(lista[1:])
+
+# si colocamos solo el final imprimira desde el inicio hasta dondes se le coloque el limite 
+print(lista[:3])
+
+# si colocamos en negatico nos imprimira desde el final hacia adelante
+print(lista[-1])
