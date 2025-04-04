ae293fff8bad1d801adb23cbb7decdedce0eb547
Jhonatan Araujo <jhona01230123@gmail.com>
2025-04-04 09:09:00 -0400

    actualizacion

---
+++ curso/recetario/entrada_salida.py
@@ -0,0 +1,36 @@
+
+'''
+usando las cuatro fundamentales
+'''
+
+mi_archivo = open("D:/clases Git y GIT HUB/PY/curso/recetario/prueba.txt")
+
+# imprime el texto dentro del archivo txt
+todo = mi_archivo.read()
+print(todo)
+
+# imprime solo la primera linea del archivo
+una_linea = mi_archivo.readline()
+print(una_linea)
+
+# imprime sin el salto que realiza al imprimir
+una_linea = (mi_archivo.rstrip())
+print(una_linea)
+
+# imprime la linea pero en mayusculas
+una_linea = (mi_archivo.upper())
+print(una_linea)
+
+# con un for
+
+for l in mi_archivo:
+    print("aqui dice: " + l)
+
+# para crear una lista con todos los elementos del archivo o todas las lineas
+todas = mi_archivo.readlines()
+
+# siempre que se trabaje con la apertura de una archivo
+# lo debemos cerrar para resguardar el espacio de memoria
+# del ordenador
+
+mi_archivo.close()
---
+++ curso/recetario/manipulacion_archivos.py
@@ -0,0 +1,11 @@
+'''
+Manipulacion de archivos desde python 
+
+E / S = Entrada / Salida
+I / O = iNPUT / OUTPUT
+
+abrir       = open()
+leer        = read()
+escribir    = write()
+cerrar      = close()
+'''
